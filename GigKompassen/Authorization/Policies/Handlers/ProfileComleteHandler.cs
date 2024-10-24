f42429854e8c08f557c6ce86a9cf1085d86642f6
Dante <dantecavallin@gmail.com>
2024-10-24 20:49:36 +0200

    Added Authorization requirement for completed profiles

---
+++ GigKompassen/Authorization/Policies/Handlers/ProfileComleteHandler.cs
@@ -0,0 +1,41 @@
+﻿using GigKompassen.Authorization.Policies.Requirements;
+using GigKompassen.Models.Accounts;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigKompassen.Authorization.Policies.Handlers
+{
+  public class ProfileComleteHandler : AuthorizationHandler<ProfileCompleteRequirement>
+  {
+
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public ProfileComleteHandler(UserManager<ApplicationUser> userManager)
+    {
+      _userManager = userManager;
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ProfileCompleteRequirement requirement)
+    {
+      if (context.User.Identity.IsAuthenticated)
+      {
+        var user = await _userManager.GetUserAsync(context.User);
+        if (user != null && user.ProfileCompleted)
+        {
+          context.Succeed(requirement);
+        }
+        else
+        {
+          context.Fail();
+        }
+      }
+    }
+  }
+}
---
+++ GigKompassen/Authorization/Policies/Requirements/ProfileCompleteRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigKompassen.Authorization.Policies.Requirements
+{
+  public class ProfileCompleteRequirement : IAuthorizationRequirement
+  {
+  }
+}
