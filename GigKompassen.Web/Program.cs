117b6a9986d60f33eae1fa9e53897ea0c1280218
Dante <dantecavallin@gmail.com>
2024-10-24 20:53:45 +0200

    Updated program.cs

--- GigKompassen.Web/Program.cs
+++ GigKompassen.Web/Program.cs
@@ -1,10 +1,12 @@
-using EventKalendern.Core.Services;
-
+using GigKompassen.Authorization.Policies.Handlers;
+using GigKompassen.Authorization.Policies.Requirements;
 using GigKompassen.Enums;
 using GigKompassen.Models.Accounts;
+using GigKompassen.Services;
 using GigKompassen.Settings;
-using GigKompassen.Web.Data;
+using GigKompassen.Data;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -13,34 +15,45 @@ namespace GigKompassen.Web
 {
   public class Program
   {
-    public static async void Main(string[] args)
+    public static async Task Main(string[] args)
     {
       var builder = WebApplication.CreateBuilder(args);
 
+      var assemblyname = typeof(Program).Assembly.GetName().Name;
       // Add services to the container.
       var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
       builder.Services.AddDbContext<ApplicationDbContext>(options =>
-          options.UseSqlServer(connectionString));
+          options.UseSqlServer(connectionString, 
+          sqlOptions => sqlOptions.MigrationsAssembly(assemblyname))
+          );
 
       builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-        {
-          options.SignIn.RequireConfirmedAccount = true;
-        })
+      {
+        options.SignIn.RequireConfirmedAccount = true;
+      })
+      .AddUserManager<UserManager<ApplicationUser>>()
+      .AddRoleManager<RoleManager<ApplicationRole>>()
       .AddEntityFrameworkStores<ApplicationDbContext>()
       .AddDefaultUI()
       .AddDefaultTokenProviders();
+
+
 
       builder.Services.AddDatabaseDeveloperPageExceptionFilter();
       AddConfiguration(builder.Configuration, builder.Services);
       ConfigureServices(builder.Services);
 
+      builder.Services.AddAuthorization(options =>
+      {
+        options.AddPolicy("ProfileComplete", policy => policy.Requirements.Add(new ProfileCompleteRequirement()));
+      });
 
       builder.Services.AddControllersWithViews();
 
       var app = builder.Build();
 
       await CreateRoles(app.Services);
-      
+
       // Configure the HTTP request pipeline.
       if (app.Environment.IsDevelopment())
       {
@@ -53,12 +66,16 @@ namespace GigKompassen.Web
         app.UseHsts();
       }
 
+      //Custom middleware
+
       app.UseHttpsRedirection();
       app.UseStaticFiles();
 
       app.UseRouting();
 
       app.UseAuthentication();
+
+      
       app.UseAuthorization();
 
       app.MapControllerRoute(
@@ -79,18 +96,34 @@ namespace GigKompassen.Web
     public static void ConfigureServices(IServiceCollection services)
     {
       services.AddTransient<IEmailSender, GmailService>();
+
+      services.AddScoped<IAuthorizationHandler, ProfileComleteHandler>();
+
+      services.AddTransient<IdentityProfileService>();
+      services.AddTransient<MediaService>();
+      services.AddTransient<GenreService>();
+      services.AddTransient<ArtistService>();
+      services.AddTransient<ManagerProfileService>();
+      services.AddTransient<SceneProfileService>();
+    
+      
     }
 
     public static async Task CreateRoles(IServiceProvider serviceProvider)
     {
-      var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
-
-      foreach (var roleName in Enum.GetNames(typeof(ApplicationRoleTypes)))
+      using (var scope = serviceProvider.CreateScope())
       {
-        if (!await roleManager.RoleExistsAsync(roleName))
+
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+        foreach (var roleName in Enum.GetNames(typeof(ApplicationRoleTypes)))
         {
-          await roleManager.CreateAsync(new ApplicationRole(roleName));
+          if (!await roleManager.RoleExistsAsync(roleName))
+          {
+            await roleManager.CreateAsync(new ApplicationRole(roleName));
+          }
         }
+
       }
     }
   }
