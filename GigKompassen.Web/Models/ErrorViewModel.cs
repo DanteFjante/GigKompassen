feaae43a7572d65dbf7dfd658c6fe8b620a096a4
Dante <dantecavallin@gmail.com>
2024-11-20 19:00:23 +0100

    Finally removed Gigkompassen.Web

--- GigKompassen.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GigKompassen.Web.Models
-{
-  public class ErrorViewModel
-  {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
