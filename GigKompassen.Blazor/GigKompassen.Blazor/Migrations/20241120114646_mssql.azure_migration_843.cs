1c3b7941b0dbbc284855173e5eeb14daeffa1940
Dante <dantecavallin@gmail.com>
2024-11-20 19:03:56 +0100

    Made sure that changes in the core package is reflected on the code in the blazor server package. And migrational files. Azure sql server is finally working.

---
+++ GigKompassen.Blazor/GigKompassen.Blazor/Migrations/20241120114646_mssql.azure_migration_843.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GigKompassen.Blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqlazure_migration_843 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
